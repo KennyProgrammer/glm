9d3e8ef819c8352bc3ae18b086afe0051413f61c
Danil <kennytuts@gmail.com>
2023-11-22 01:47:22 +0300

    Add Glm.lua file.

---
+++ Glm.lua
@@ -0,0 +1,40 @@
+--
+-- Main Premake5 file for building Glm project.
+-- Copyright (c) 2020-2023 by Danil (Kenny) Dukhovenko, All rights reserved.
+-- 
+-- Requirement:
+--  - ForceEngine.lua
+--
+
+-- Glm C++ Project
+project "Glm"
+	kind          "StaticLib"
+	language      "C++"
+	cppdialect    "C++11"
+	staticruntime "on"
+	targetdir     ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}")
+	objdir        ("%{ForceDir.BinLib}/" .. BuildDir .. "/%{prj.name}")
+
+	files {
+		"include/glm/**.hpp",
+        "include/glm/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs {
+		"include"
+	}
+
+	filter "system:windows" {
+		systemversion "latest"
+	}
+
+	filter "configurations:Debug" {
+		runtime "Debug",
+		symbols "on"
+	}
+
+	filter "configurations:Release" {
+		runtime  "Release",
+		optimize "on"
+	}
